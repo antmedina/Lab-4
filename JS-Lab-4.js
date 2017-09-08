57383d61732f058116f91013b953bff3cba4adc1
antmedina <ant.n.medina@gmail.com>
2017-09-07 22:06:33 -0400

    FFFFFAAAA....

--- JS-Lab-4.js
+++ JS-Lab-4.js
@@ -1,14 +1,14 @@
 //create color array
 var RYGB=["red","yellow","blue","green"];
-//create function randoColor
-function randoColor(event){
-    var index = Math.floor(Math.random()*RYGB.length);
-    var newColor = colors[index];
-// event.target.style.backgroundColor = newColor;
+// //create function randoColor
+// function randoColor(event){
+//     var index = Math.floor(Math.random()*RYGB.length);
+//     var newColor = colors[index];
+// // event.target.style.backgroundColor = newColor;
 
-// We need to set newColor as the backgroundColor of the clicked div; no need if add event parameter to function + .target class       
-   this.style.backgroundColor = newColor;
-     }
+// // We need to set newColor as the backgroundColor of the clicked div; no need if add event parameter to function + .target class       
+//    this.style.backgroundColor = newColor;
+//      }
 //created button
 document.addEventListener("DOMContentLoaded" , function(){
     var btn =document.createElement("button");
@@ -17,23 +17,55 @@ document.addEventListener("DOMContentLoaded" , function(){
     document.body.appendChild(btn);
 //add event
     btn.addEventListener("click" , addSquare);
+
 //template for squares
     // var square = document.createElement('div');
     // square.className = 'square';
     // document.body.appendChild(square);
+
 //create function for addSquare
     function addSquare(){
+         var existingSquares = document.getElementsByClassName('squares').length;
         var squares = document.createElement("div");
         squares.className = "squares";
-        // squares.id=existingSquares;
-        // var existingSquares = document.getElementsByClassName('squares').length;
+        squares.id=existingSquares;
+       
+ 
  //create eventListeners for squares       
-        squares.addEventListener("click" , randoColor);
-        // squares.addEventListener("dblclick" , removeNeighbor);
-        // squares.addEventListener("mouseover" , showNum);
-        // squares.addEventListener("mouseleave" , removeNum);
+        // squares.addEventListener("click" , randoColor);
+        squares.addEventListener("dblclick" , removeNeighbor);
+        squares.addEventListener("mouseover" , showNum);
+        squares.addEventListener("mouseleave" , removeNum);
 
         document.body.appendChild(squares);
     }
+    function showNum() {
+        var id = this.id;
+        this.innerText = id;
+    }
+    function removeNum() {
+        this.innerText = "";
+    }
+    function removeNeighbor() {
+        var id = this.id;
+        if (id % 2 === 0) {
+        // if even, remove the square after
+             if (this.nextSibling) {
+                 this.nextSibling.remove();
+             } else {
+                 alert('There are no elements to the right to remove.');
+             }
+        } else {
+        // if odd, remove the square before
+             if (this.previousSibling) {
+            this.previousSibling.remove();
+             } else {
+            alert('There are no elements to the left to remove.');
+             }
+             if (id===0){
+                
+             }
+        }
+     }
     
 })
